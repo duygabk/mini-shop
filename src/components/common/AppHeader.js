5766b214d6b6c7219177e0e807976388508d321a
duygabk <45941584+duygabk@users.noreply.github.com>
2020-08-18 13:39:36 +0900

    codesandbox miss

--- src/components/common/AppHeader.js
+++ src/components/common/AppHeader.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 function AppHeader(props) {
-  return <>Header</>;
+  return (
+    <div className="jumbotron" style={{ marginBottom: 0 }}>
+      <h1 className="display-4">Hello, world!</h1>
+      <p className="lead">
+        This is a simple hero unit, a simple jumbotron-style component for
+        calling extra attention to featured content or information....
+      </p>
+      <hr className="my-4" />
+    </div>
+  );
 }
 
 export default AppHeader;
